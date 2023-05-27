833bca7552feb870ca4d7ef5ed3ec66834728040
İlker Karaca <ikaracadeveloper@gmail.com>
2023-05-27 10:56:27 +0300

    f"{} {}" applied

--- C2P18.py
+++ C2P18.py
@@ -4,3 +4,8 @@ messageTwo="Message two"
 print(messageTwo)
 messageTwo="message two changed"
 print(messageTwo.title())
+##########################################
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
