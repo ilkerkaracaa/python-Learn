1bd2d27a74625759cb558dbea1b04543f1ef92b7
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-07 18:33:09 +0300

    pythonda denemeler yapıyorum

--- C2.py
+++ C2.py
@@ -1,16 +1,75 @@
-message = "Hello Python Crash Course reader!"
-print(message)
-messageTwo="Message two" 
-print(messageTwo)
-messageTwo="message two changed"
-print(messageTwo.title())
-##########################################
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(full_name.title())
-print(f"Merhaba, {full_name.title()}")
+# message = "Hello Python Crash Course reader!"
+# print(message)
+# messageTwo="Message two" 
+# print(messageTwo)
+# messageTwo="message two changed"
+# print(messageTwo.title())
+# ##########################################
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(full_name.title())
+# print(f"Merhaba, {full_name.title()}")
 
-favorite_language = 'python '
-print(favorite_language)
-print(favorite_language.rstrip())
+# favorite_language = 'python '
+# print(favorite_language)
+# print(favorite_language.rstrip())
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[-2])
+
+# cars = ['bmw', 'badi', 'toyota', 'subaru'] 
+# cars.sort()
+# print(cars)
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#   print(f"{magician.title()}, that was a great trick!")
+#   print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#   print(f"{magician.title()}, that was a great trick!") 
+# print(f"I can't wait to see your next trick, {magician.title()}.\n")
+# print(magician)
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+#     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+# print("Thank you everyone, that was a great magic show!")
+
+# numbers = set(range(1, 6))
+# print(numbers)
+
+
+# numbers = list(range(1, 6))
+# print(numbers)
+
+# squares = []
+# for value in range(1,11):
+#   squares.append(value**2) 
+# print(squares)
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[-3:])
+# my_t = (3,)
+# print(my_t[1])
+
+# alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0)
+# alien_0['x_position'] = 0 
+# alien_0['y_position'] = 25
+# print(alien_0)  
+# del alien_0['points']
+
+
+# user_0 = {
+# 'username': 'efermi',
+# 'first': 'enrico',
+# 'last': 'fermi',
+# }
+# print(user_0.items())
