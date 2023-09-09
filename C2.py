f675b50de51edcc5cc51dc6ee1946b8a832fd93b
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-09 19:46:50 +0300

    wooow classes are awesome

--- C2.py
+++ C2.py
@@ -243,27 +243,27 @@
 #     def increment_odometer(self, miles):
 #         self.odometer_reading += miles
                  
-class Battery:
-    def __init__(self, battery_size=75):
-        self.battery_size = battery_size
-    def upgrade_battery(self):
-        if self.battery_size != 100:
-            self.battery_size = 100
-    def describe_battery(self):
-        print(f"This car has a {self.battery_size}-kWh battery.")
-    def get_range(self):
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-        print(f"This car can go about {range} miles on a full charge.")
+# class Battery:
+#     def __init__(self, battery_size=75):
+#         self.battery_size = battery_size
+#     def upgrade_battery(self):
+#         if self.battery_size != 100:
+#             self.battery_size = 100
+#     def describe_battery(self):
+#         print(f"This car has a {self.battery_size}-kWh battery.")
+#     def get_range(self):
+#         if self.battery_size == 75:
+#             range = 260
+#         elif self.battery_size == 100:
+#             range = 315
+#         print(f"This car can go about {range} miles on a full charge.")
             
-class ElectricCar():
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery = Battery()
-    def fill_gas_tank(self):
-        print("This car doesn't need a gas tank!")
+# class ElectricCar(Car):
+#     def __init__(self, make, model, year):
+#         super().__init__(make, model, year)
+#         self.battery = Battery()
+#     def fill_gas_tank(self):
+#         print("This car doesn't need a gas tank!")
                 
 # my_tesla = ElectricCar('tesla', 'model s', 2019)
 # print(my_tesla.get_descriptive_name())
