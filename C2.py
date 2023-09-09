8a3126b5faadc56dc6fed15e5ca8030aff7af7c9
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-09 19:31:19 +0300

    classes and instances

--- C2.py
+++ C2.py
@@ -223,27 +223,25 @@
 # my_dog.sit()
 # my_dog.roll_over()
 
-class Car:
-# A simple attempt to represent a car.
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-    def fill_gas_tank(self):
-        print("This car needs a gas tank!")
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+# class Car:
+# # A simple attempt to represent a car.
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it.")
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back an odometer!")
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
                  
 class Battery:
     def __init__(self, battery_size=75):
